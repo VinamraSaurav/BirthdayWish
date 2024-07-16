81c71d5c057e55f71f4ff1ccf12e33e92dcc66cf
VinamraSaurav <80186642+VinamraSaurav@users.noreply.github.com>
2024-07-17 02:09:36 +0530

    almost completed

--- script/main.js
+++ script/main.js
@@ -225,7 +225,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "#c24461",
         ease: Expo.easeOut,
       },
       0.1,
